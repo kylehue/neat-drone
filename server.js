ceec6fb36b4c9283aa0e3212c899e36493d83bd6
kylehue <apeuda69@gmail.com>
2021-05-27 06:16:22 +0800

    update

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require("express");
 const app = express();
 const port = 8080;
 app.listen(port, () => console.log(`listening at ${port}`));
-app.use(express.static("docs"));
+app.use(express.static(__dirname + "/docs"));
